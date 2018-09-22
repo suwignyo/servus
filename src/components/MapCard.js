0fe255df9c0c5df5686eb5be6a75d61877cf9198
suwignyo <gsuwignyo@gmail.com>
2018-09-22 17:47:45 -0400

    styled services card

--- src/components/MapCard.js
+++ src/components/MapCard.js
@@ -1,39 +1,40 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
+import "../styles/Map.css";
 
 class MapCard extends Component {
-    componentDidMount() {
-        console.log(this.props.data.img);
-    }
-    render() {
-        return (
-            <div>
-                <div className="listing-map-card">
-                    <div className="listing-map-img-container">
-                        <img src={this.props.data.img} alt="doctor image" />
-                    </div>
-                    <div className="listing-map-info-container">
-                        <div>
-                            <div className="doctor-name">
-                                {this.props.data.name}
-                            </div>
-                            <div className="distance-away">500m</div>
-                        </div>
-                        <div>
-                            <div className="service">
-                                {this.props.data.service}
-                            </div>
-                            <div className="languages-heading">Languages</div>
-                        </div>
-                        <div className="langauges-spoken">
-                            {this.props.data.languages.join(", ")}
-                        </div>
-                        <div>{this.props.data.office}</div>
-                    </div>
-                </div>
+  componentDidMount() {
+    console.log(this.props.data.img);
+  }
+  render() {
+    return (
+      <div>
+        <div className="listing-map-card">
+          <div className="listing-map-img-container">
+            <img
+              className="prof-pic"
+              src={this.props.data.img}
+              alt="doctor image"
+            />
+          </div>
+          <div className="listing-map-info-container">
+            <div className="doctor-distance">
+              <div className="doctor-name">{this.props.data.name}</div>
+              <div className="distance-away">500m</div>
             </div>
-        );
-    }
+            <div className="service-language">
+              <div className="service">{this.props.data.service}</div>
+              <div className="languages-heading">Languages</div>
+            </div>
+            <div className="languages-spoken">
+              {this.props.data.languages.join(", ")}
+            </div>
+            <div className="office">{this.props.data.office}</div>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default MapCard;
