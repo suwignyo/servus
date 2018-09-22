7f7da89542b21cded69a529c5ebd4e0fb813e4e4
suwignyo <gsuwignyo@gmail.com>
2018-09-22 15:29:46 -0400

    styled search box

--- src/components/SearchBox/SelectService.js
+++ src/components/SearchBox/SelectService.js
@@ -1,111 +1,107 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-
+// import cultural from "../../assets/community.png";
 //actions
 import { setService } from "../../actions/userActions";
 
 class SelectService extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            category: ""
-        };
+  constructor(props) {
+    super(props);
+    this.state = {
+      category: ""
+    };
+  }
+  handleCategoryClick = category => {
+    this.setState({ category });
+  };
+  handleSelectService = service => {
+    this.props.setService(service);
+  };
+  clearCategory = () => {
+    this.setState({ category: "" });
+  };
+  render() {
+    let services;
+    if (this.state.category === "Cultural") {
+      services = (
+        <div className="service-box-container">
+          <div
+            className="service-box worship"
+            onClick={() => this.handleSelectService("Place of Worship")}
+          >
+            <span>Place of Worship</span>
+          </div>
+          <div
+            className="service-box temples"
+            onClick={() => this.handleSelectService("Temples")}
+          >
+            <span>Temples</span>
+          </div>
+          <div
+            className="service-box markets"
+            onClick={() => this.handleSelectService("Local Markets")}
+          >
+            <span>Local Markets</span>
+          </div>
+        </div>
+      );
+    } else if (this.state.category === "Professional") {
+      services = (
+        <div className="service-box-container">
+          <div
+            className="service-box doctors"
+            onClick={() => this.handleSelectService("Doctors")}
+          >
+            <span>Doctors</span>
+          </div>
+          <div
+            className="service-box lawyers"
+            onClick={() => this.handleSelectService("Lawyers")}
+          >
+            <span>Lawyers</span>
+          </div>
+          <div
+            className="service-box accountants"
+            onClick={() => this.handleSelectService("Accountants")}
+          >
+            <span>Accountants</span>
+          </div>
+        </div>
+      );
     }
-    handleCategoryClick = category => {
-        this.setState({ category });
-    };
-    handleSelectService = service => {
-        this.props.setService(service);
-    };
-    clearCategory = () => {
-        this.setState({ category: "" });
-    };
-    render() {
-        let services;
-        if (this.state.category === "Cultural") {
-            services = (
-                <div className="service-box-container">
-                    <div
-                        className="service-box"
-                        onClick={() =>
-                            this.handleSelectService("Place of Worship")
-                        }
-                    >
-                        <span>Place of Worship</span>
-                    </div>
-                    <div
-                        className="service-box"
-                        onClick={() => this.handleSelectService("Temples")}
-                    >
-                        <span>Temples</span>
-                    </div>
-                    <div
-                        className="service-box"
-                        onClick={() =>
-                            this.handleSelectService("Local Markets")
-                        }
-                    >
-                        <span>Local Markets</span>
-                    </div>
-                </div>
-            );
-        } else if (this.state.category === "Professional") {
-            services = (
-                <div className="service-box-container">
-                    <div
-                        className="service-box"
-                        onClick={() => this.handleSelectService("Doctors")}
-                    >
-                        <span>Doctors</span>
-                    </div>
-                    <div
-                        className="service-box"
-                        onClick={() => this.handleSelectService("Lawyers")}
-                    >
-                        <span>Lawyers</span>
-                    </div>
-                    <div
-                        className="service-box"
-                        onClick={() => this.handleSelectService("Accountants")}
-                    >
-                        <span>Accountants</span>
-                    </div>
-                </div>
-            );
-        }
-        return (
-            <div>
-                {this.state.category && (
-                    <span onClick={this.clearCategory}>X TO GO BACK</span>
-                )}
-                {this.state.category ? (
-                    <div />
-                ) : (
-                    <div className="service-box-container">
-                        <div
-                            className="service-box"
-                            onClick={() => this.handleCategoryClick("Cultural")}
-                        >
-                            <span>Cultural</span>
-                        </div>
-                        <div
-                            className="service-box"
-                            onClick={() =>
-                                this.handleCategoryClick("Professional")
-                            }
-                        >
-                            <span>Professional</span>
-                        </div>
-                    </div>
-                )}
-
-                {this.state.category && services}
+    return (
+      <div>
+        {this.state.category && (
+          <div className="x-button" onClick={this.clearCategory}>
+            X
+          </div>
+        )}
+        {this.state.category ? (
+          <div />
+        ) : (
+          <div className="service-box-container">
+            <div
+              className="service-box cultural"
+              onClick={() => this.handleCategoryClick("Cultural")}
+            >
+              <span>Cultural</span>
             </div>
-        );
-    }
+            <div
+              className="service-box professional"
+              onClick={() => this.handleCategoryClick("Professional")}
+            >
+              <span>Professional</span>
+            </div>
+          </div>
+        )}
+
+        {this.state.category && services}
+      </div>
+    );
+  }
 }
 
 export default connect(
-    null,
-    { setService }
+  null,
+  { setService }
 )(SelectService);
--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -5,25 +5,27 @@ import SelectLanguage from "./SelectLanguage";
 import SelectService from "./SelectService";
 
 export default class index extends Component {
-    render() {
-        return (
-            <div className="search-box">
-                <div>
-                    <p>Language</p>
-                    <SelectLanguage />
-                </div>
-                <div>
-                    <p>Service</p>
-                    <SelectService />
-                </div>
-                <div>
-                    <p>Location</p>
-                    <input />
-                </div>
-                <div className="find-button">
-                    <p>Find</p>
-                </div>
-            </div>
-        );
-    }
+  render() {
+    return (
+      <div className="search-box">
+        <div className="search-box-selects">
+          <div>
+            <p>Language</p>
+            <SelectLanguage />
+          </div>
+          <div>
+            <p>Service</p>
+            <SelectService />
+          </div>
+          <div>
+            <p>Location</p>
+            <input className="input-box" />
+          </div>
+          <div className="find-button">
+            <button className="search-button">FIND</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
