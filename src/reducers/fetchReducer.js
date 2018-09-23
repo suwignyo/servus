9b8cbaced2ea2ce39130b491075fa516f37b45d7
teeaaspoon <tommypoon36@gmail.com>
2018-09-23 11:02:20 -0400

    added lawyers

--- src/reducers/fetchReducer.js
+++ src/reducers/fetchReducer.js
@@ -1,7 +1,7 @@
 import { FETCH_RESULTS } from "../actions/types";
 
 const initialState = {
-    results: [
+    doctors: [
         {
             id: 1,
             name: "Dr. Angela Wong",
@@ -13,7 +13,8 @@ const initialState = {
             hours: "Open - Closes 6pm",
             phone: "416-977-8878",
             website: "N/A",
-            img: "https://healthy.kaiserpermanente.org/health/PA_MyDoctor/PhotoProvider?id=9481791",
+            img:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzecKpjF2lEJuZltsDSDCkXIRyQTHqiu7squgMXr2XboUpyopy",
             lng: -79.385384,
             lat: 43.660645
         },
@@ -28,7 +29,8 @@ const initialState = {
             hours: "Open - Closes 6pm",
             phone: "647-427-6001",
             website: "http://www.downtowntorontodoctors.com",
-            img: "https://healthy.kaiserpermanente.org/health/PA_MyDoctor/PhotoProvider?id=9481791",
+            img:
+                "https://drmargaretliu.com/wp-content/uploads/2015/03/DSCF4248-e1425870594681.jpg",
             lng: -79.385948,
             lat: 43.660506
         },
@@ -43,7 +45,8 @@ const initialState = {
             hours: "Closed - Opens 9 a.m. Mon",
             phone: "416-515-8808",
             website: "https://baydermatologycentre.com",
-            img: "https://healthy.kaiserpermanente.org/health/PA_MyDoctor/PhotoProvider?id=9481791",
+            img:
+                "https://www.unitedvetclinic.com/wp-content/uploads/2018/05/UVC-Dr-Talitha-Tumboimbela-.jpg",
             lng: -79.385948,
             lat: 43.660696
         },
@@ -58,25 +61,107 @@ const initialState = {
             hours: "Open - Closes 6pm",
             phone: "416-551-6006",
             website: "https://www.torendoscopy.ca/",
-            img: "https://healthy.kaiserpermanente.org/health/PA_MyDoctor/PhotoProvider?id=9481791",
+            img:
+                "https://d1cesmq0xhh7we.cloudfront.net/a6f48e86-7376-47f6-85f1-f9416987c601zoom.jpg",
             lng: -79.385999,
             lat: 43.660606
         },
         {
-          id: 5,
-          name: "Dr. Benny Chang",
-          service: "Family Medicine",
-          office: "Maple Leaf Medical Clinic",
-          distance: "500m away",
-          languages: ["English", "Cantonese", "Mandarin"],
-          address: "14 College St, Toronto, ON M5G 1K2",
-          hours: "Closed - Opens 12 p.m. Mon.",
-          phone: "416-465-3252",
-          website: "https://www.mlmedical.com/",
-          img: "https://healthy.kaiserpermanente.org/health/PA_MyDoctor/PhotoProvider?id=9481791",
-          lng: -79.383942,
-          lat: 43.661488
-      },
+            id: 5,
+            name: "Dr. Benny Chang",
+            service: "Family Medicine",
+            office: "Maple Leaf Medical Clinic",
+            distance: "500m away",
+            languages: ["English", "Cantonese", "Mandarin"],
+            address: "14 College St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 12 p.m. Mon.",
+            phone: "416-465-3252",
+            website: "https://www.mlmedical.com/",
+            img: "https://directory.wkhs.com/Photos/11113096.jpg",
+            lng: -79.383942,
+            lat: 43.661488
+        }
+    ],
+    lawyers: [
+        {
+            id: 6,
+            name: "Kai Deng",
+            service: "Barrister & Solicitor",
+            office: "Kai Deng Law Office",
+            distance: "350m away",
+            languages: ["English", "Mandarin"],
+            address: "14 College St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 8 a.m. Mon.",
+            phone: "416-465-3252",
+            website: "http://www.dhdlawyer.com/kai-deng-law-office/",
+            img:
+                "http://3.bp.blogspot.com/-9ax2ralS1IY/U-2FheTiu7I/AAAAAAAAADs/E1H54a9rq1Q/s1600/89120%E4%BF%AE.jpg",
+            lng: -79.393139,
+            lat: 43.65955
+        },
+        {
+            id: 7,
+            name: "Zhen Dong Sun",
+            service: "Attorney",
+            office: "Z. Sun Law Professional",
+            distance: "400m away",
+            languages: ["English", "Mandarin"],
+            address: "77 Gerrard St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 8 a.m. Mon.",
+            phone: "416-465-3252",
+            website: "http://www.dhdlawyer.com/kai-deng-law-office/",
+            img:
+                "https://pbs.twimg.com/profile_images/541832964292624384/XYfxLT2T.jpeg",
+            lng: -79.389872,
+            lat: 43.657198
+        },
+        {
+            id: 8,
+            name: "Jack Yu",
+            service: "Corporate Law",
+            office: "Jack Yu Law Office",
+            distance: "400m away",
+            languages: ["English", "Mandarin", "Cantonese"],
+            address: "600 Bay St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 8 a.m. Mon.",
+            phone: "416-465-3252",
+            website: "http://www.dhdlawyer.com/kai-deng-law-office/",
+            img:
+                "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNA8jLuOk720EJEQQ3L8w9blTfKM02YlST6nkhO8Tyce6QCe6Z5Q",
+            lng: -79.385561,
+            lat: 43.659294
+        },
+        {
+            id: 9,
+            name: "Melinda Zhang",
+            service: "Barrister & Solicitor",
+            office: "Radnoff Law Office",
+            distance: "400m away",
+            languages: ["English", "Mandarin", "French"],
+            address: "14 College St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 8 a.m. Mon.",
+            phone: "416-465-3252",
+            website: "https://www.fasken.com/en/jack-yu",
+            img: "https://www.chineselawyers.com/images/melinda-zhang-2006",
+            lng: -79.386044,
+            lat: 43.660435
+        },
+        {
+            id: 10,
+            name: "Ella Chen",
+            service: "Criminal Defense Lawyer",
+            office: "Ella Chen LLP",
+            distance: "500m away",
+            languages: ["English", "Mandarin", "Korean"],
+            address: "14 College St, Toronto, ON M5G 1K2",
+            hours: "Closed - Opens 8 a.m. Mon.",
+            phone: "416-465-3252",
+            website: "http://www.dhdlawyer.com/kai-deng-law-office/",
+            img:
+                "https://upload.wikimedia.org/wikipedia/commons/1/1b/%E8%94%A1%E8%8B%B1%E6%96%87%E5%AE%98%E6%96%B9%E5%85%83%E9%A6%96%E8%82%96%E5%83%8F%E7%85%A7.png",
+            lng: -79.384787,
+            lat: 43.657458
+        }
     ]
 };
 
